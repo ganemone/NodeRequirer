5bb53b48f87195e0cffc775054bb674e9732fd90
Giancarlo Anemone <ganemone@gmail.com>
2015-03-13 15:37:31 -0400

    Fix linting errors and docstring formatting

--- src/utils.py
+++ src/utils.py
@@ -11,6 +11,7 @@ SETTINGS_FILE = "NodeRequirer.sublime-settings"
 
 MERGE_BLACKLIST = ('omit_extensions',)
 
+
 def merge_pref(key, old_val, new_val):
     if new_val is None:
         return old_val
@@ -44,9 +45,11 @@ def get_project_pref(key, view=None):
         # Allow project .noderequirerrc files to override preferences
         rcfile = findup(view.file_name(), '.noderequirer.json')
         if rcfile:
-            val = merge_pref(key, val, json.load(open(rcfile, 'r', encoding='UTF-8')).get(key))
+            pref = json.load(open(rcfile, 'r', encoding='UTF-8')).get(key)
+            val = merge_pref(key, val, pref)
 
-        # Allow per-project preferences from the project file to override preferences and project rc settings
+        # Allow per-project preferences from the project file to override
+        # preferences and project rc settings
         project_settings = view.window().project_data().get('NodeRequirer')
         if project_settings:
             val = merge_pref(key, val, project_settings.get(key))
@@ -80,20 +83,23 @@ def aliased(module_path, view=None):
         if m:
             return m.expand(result_pattern)
 
-    # Allow the alias for package.json in any location to be defined by a "package.json" alias
+    # Allow the alias for package.json in any location to be defined by a
+    # "package.json" alias
     if os.path.basename(module_path) == 'package.json':
         if 'package.json' in aliases:
             return aliases['package.json']
 
     return None
 
-def strip_snippet_groups(snippet_text):
-    """
-    This (admittedly complex looking) function goes through a snippet string and strips
-    out the groups (`${{1}}` and `${{2:content}}`) leaving any default text in their place.
 
-    It's somewhat complex since it iterates through matches on `${{n}}`, `${{n:`, and `}}`
-    and uses a stack to ensure that nested groups (`${{1:...${{2:...}}...}}`) work correctly
+def strip_snippet_groups(snippet_text):
+    """This (admittedly complex looking) function goes through a snippet
+    string and strips out the groups (`${{1}}` and `${{2:content}}`)
+    leaving any default text in their place.
+
+    It's somewhat complex since it iterates through matches
+    on `${{n}}`, `${{n:`, and `}}` and uses a stack to ensure that
+    nested groups (`${{1:...${{2:...}}...}}`) work correctly
     and `}}`'s that do not belong to a group are not discarded.
     """
     r = re.compile('\$\{\{(\d+)(:|\}\})|(\}\})')
@@ -136,60 +142,62 @@ def strip_snippet_groups(snippet_text):
 
 
 def lazy_parse_comment_json(json_text):
-	"""
-	This attempts to parse files like .jscsrc which are .json files with
-	comments in them.
+    """Attempts to parse files like .jscsrc which are .json files with
+    comments in them. This is lazy in that we simply strip things with a
+    regexp and don't take string boundaries into account.
+    This should be fine since it's unlikely that any of the config
+    options we want contain comment-like text
+    """
 
-	This is lazy in that we simply strip things with a regexp and don't take string boundaries into account.
-	This should be fine since it's unlikely that any of the config options we want contain comment-like text
-	"""
+    if isinstance(json_text, io.IOBase):
+        json_text = json_text.read()
 
-	if isinstance(json_text, io.IOBase):
-		json_text = json_text.read()
-
-	json_text = re.sub(r'(#|//).*$', '', json_text, re.MULTILINE)
-	json_text = re.sub(r'/\*.*\*/', '', json_text, re.DOTALL)
-	return json.loads(json_text)
+    json_text = re.sub(r'(#|//).*$', '', json_text, re.MULTILINE)
+    json_text = re.sub(r'/\*.*\*/', '', json_text, re.DOTALL)
+    return json.loads(json_text)
 
 
 def get_jscs_options(path):
-	option_sets = []
+    option_sets = []
 
-	jscsrc_path = findup(path, '.jscsrc')
-	if os.path.isfile(jscsrc_path):
-		jscsrc = lazy_parse_comment_json(open(jscsrc_path, 'r', encoding='UTF-8'))
-		option_sets.append((jscsrc_path, jscsrc))
+    jscsrc_path = findup(path, '.jscsrc')
+    if os.path.isfile(jscsrc_path):
+        jscsrc = lazy_parse_comment_json(
+            open(jscsrc_path, 'r', encoding='UTF-8'))
+        option_sets.append((jscsrc_path, jscsrc))
 
-	jscs_json_path = findup(path, '.jscs.json')
-	if os.path.isfile(jscs_json_path):
-		jscs_json = json.load(open(jscs_json_path, 'r', encoding='UTF-8'))
-		option_sets.append((jscs_json_path, jscs_json))
+    jscs_json_path = findup(path, '.jscs.json')
+    if os.path.isfile(jscs_json_path):
+        jscs_json = json.load(open(jscs_json_path, 'r', encoding='UTF-8'))
+        option_sets.append((jscs_json_path, jscs_json))
 
-	package_path = findup(path, 'package.json')
-	if os.path.isfile(package_path):
-		package = json.load(open(package_path, 'r', encoding='UTF-8'))
-		if 'jscsConfig' in package:
-			option_sets.append((package_path, package['jscsConfig']))
+    package_path = findup(path, 'package.json')
+    if os.path.isfile(package_path):
+        package = json.load(open(package_path, 'r', encoding='UTF-8'))
+        if 'jscsConfig' in package:
+            option_sets.append((package_path, package['jscsConfig']))
 
-	# Sort sets by dirname length
-	option_sets.sort(key=lambda x: len(os.path.dirname(x[0])))
+    # Sort sets by dirname length
+    option_sets.sort(key=lambda x: len(os.path.dirname(x[0])))
 
-	# Merge options together
-	options = dict()
-	for path, option_set in option_sets:
-		options.update(option_set)
+    # Merge options together
+    options = dict()
+    for path, option_set in option_sets:
+        options.update(option_set)
 
-	return options
+    return options
+
 
 def findup(path, relative_path):
-	path = os.path.abspath(path)
-	# Testing path against dirname(path) should be more reliable than testing it against '/'
-	# and work on Windows where root may be C:/ or something else.
-	while path and path != os.path.dirname(path):
-		test_path = os.path.join(path, relative_path)
-		if os.path.isfile(test_path):
-			return test_path
+    path = os.path.abspath(path)
+    # Testing path against dirname(path) should be more reliable than
+    # testing it against '/' and works on Windows where root may be C:/
+    # or something else.
+    while path and path != os.path.dirname(path):
+        test_path = os.path.join(path, relative_path)
+        if os.path.isfile(test_path):
+            return test_path
 
-		path = os.path.dirname(path)
+        path = os.path.dirname(path)
 
-	return False
+    return False
